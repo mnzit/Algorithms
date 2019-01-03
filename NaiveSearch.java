2643ffc2a1e9441ec91146198a4deec457fb3093
mnzit <mnzitshakya@gmail.com>
2019-01-03 20:04:29 +0545

    naive search added

--- NaiveSearch.java
+++ NaiveSearch.java
@@ -13,12 +13,16 @@ package hackerrank;
  */
 public class NaiveSearch {
 
-    public void search(String search, String input) {
+    public static void search(String search, String input) {
         for (int i = 0; i < input.length() - search.length() + 1; i++) {
             String subs = input.substring(i, i + search.length());
             if (subs.equals(search)) {
                 System.out.println("Found (" + search + ") in (" + input + ") from index (" + i + " to " + (i + search.length() - 1) + ")");
             }
         }
+    }
+
+    public static void main(String[] args) {
+        search("abcabc", "abaabcaacabcabaabcabcaaacacabc");
     }
 }
