ed44529c765088c5e734e9074f1900496bad1bb6
mnzit <mnzitshakya@gmail.com>
2019-01-21 16:30:45 +0545

    GradingStudent

---
+++ GradingStudent.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+/**
+ *
+ * @author Mnzit
+ */
+public class GradingStudent {
+
+    static int[] gradingStudents(int[] grades) {
+        int count = 0;
+        for (int i = 0; i < grades.length; i++) {
+            int incre = grades[i];
+            if(!(grades[i] < 38)){
+                while (incre % 5 != 0) {
+                    incre++;
+                }
+                if (incre - grades[i] < 3) {
+                    grades[i] = incre;
+                }
+            }
+            
+        }
+
+        return grades;
+    }
+
+    public static void main(String[] args) {
+        for (int i : gradingStudents(new int[]{73, 67, 38, 33})) {
+            System.out.println(i);
+        }
+
+    }
+}
