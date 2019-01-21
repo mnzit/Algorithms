9585a233e29aab7afd8784f7f84896b5e4ca666e
mnzit <mnzitshakya@gmail.com>
2019-01-21 16:04:33 +0545

    TimeConversion

--- TimeConversion.java
+++ TimeConversion.java
@@ -13,22 +13,21 @@ public class TimeConversion {
 
     static String timeConversion(String s) {
         String temp = s.split(":")[0];
-        int result = 0;
         int j = 12;
         if (s.endsWith("PM")) {
             for (int i = 0; i < 12; i++) {
-                result = i == 0 ? 12 : i;
-                if (String.format("%02d", result).equalsIgnoreCase(temp)) {
-                    s = s.replaceFirst(temp, String.valueOf(j++)).replace("PM", "");
+                if (String.format("%02d", i == 0 ? 12 : i).equalsIgnoreCase(temp)) {
+                    s = s.replaceFirst(temp, String.valueOf(j)).replace("PM", "");
                 }
+                j++;
             }
         }else{
-            s = s.replace("AM", "").replace("12", "00");
+              s = s.replace("AM", "").replaceFirst("12", "00");
         }
         return s;
     }
 
     public static void main(String[] args) {
-        System.out.println(timeConversion("12:45:54AM"));
+        System.out.println(timeConversion("12:12:12AM"));
     }
 }
